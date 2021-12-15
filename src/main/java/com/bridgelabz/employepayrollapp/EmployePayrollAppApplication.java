6eeb769a18b1b81073b6b3e97e8ca85b90895735
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-12-15 14:30:11 +0530

    Added: Project files

---
+++ src/main/java/com/bridgelabz/employepayrollapp/EmployePayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployePayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployePayrollAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/employepayrollapp/EmployePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployePayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
