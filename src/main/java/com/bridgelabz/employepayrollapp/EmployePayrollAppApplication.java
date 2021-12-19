fcd7d74f9bd48b4e40660b53e7dec5f512b4cba7
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-12-19 14:05:56 +0530

    Added:Validation For all Feilds in EmployeePayroll Dto

--- src/main/java/com/bridgelabz/employepayrollapp/EmployePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employepayrollapp/EmployePayrollAppApplication.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employepayrollapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
@@ -9,8 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployePayrollAppApplication {
 
 	public static void main(String[] args) {
-	    SpringApplication.run(EmployePayrollAppApplication.class, args);
-		
+		ApplicationContext context = SpringApplication.run(EmployePayrollAppApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+        log.info("Employee Payroll DB User is {}",
+                context.getEnvironment().getProperty("spring.datasource.username"));
 	               
 	       
 	}
--- src/main/java/com/bridgelabz/employepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/bridgelabz/employepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.employepayrollapp.exceptions;
 
 import com.bridgelabz.employepayrollapp.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +14,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionHandler {
+    private static final String message = "Exception While Processing REST Request";
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handelHttpMessageNotReadableException(
+            HttpMessageNotReadableException exception) {
+        log.error("Invalid Date Format", exception);
+        ResponseDTO responseDTO = new ResponseDTO(message, "Should have date in dd MMM yyyy format");
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
@@ -20,17 +32,16 @@ public class EmployeePayrollExceptionHandler {
         List<String> errMesg = errorList.stream()
                 .map(objErr -> objErr.getDefaultMessage())
                 .collect(Collectors.toList());
-        ResponseDTO responseDTO = new ResponseDTO("Exception While Processing REST Request", errMesg);
+        ResponseDTO responseDTO = new ResponseDTO(message, errMesg);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(EmployeePayrollException.class)
     public ResponseEntity<ResponseDTO> handleEmployeePayrollException(
             EmployeePayrollException exception) {
-        ResponseDTO responseDTO = new ResponseDTO("Exception while REST Request",
+        ResponseDTO responseDTO = new ResponseDTO(message,
                 exception.getMessage());
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
-
-
 
 }
